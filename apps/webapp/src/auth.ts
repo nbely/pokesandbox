c477a669b2baf93a3df785329fbbf12c3408e9b0
Nick Bely <67796036+nbely@users.noreply.github.com>
2026-01-28 03:55:34 -0500

    PR feedback to fit auth guard property

--- apps/webapp/src/auth.ts
+++ apps/webapp/src/auth.ts
@@ -4,12 +4,12 @@ import Discord from "next-auth/providers/discord";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Discord],
   callbacks: {
-    async jwt({ token, profile, account, user, session }) {
+    async jwt({ token, profile }) {
       if (profile) token.id = profile.id;
       return token;
     },
-    async session({ session, token, user }) {
-      if (session.user && token.sub) session.user.id = token.id as string;
+    async session({ session, token }) {
+      if (session.user && token.id) session.user.id = token.id as string;
       return session;
     },
   },
